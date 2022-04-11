0108c2748f2e1f38bc396f2e40e2f1e3001d913e
saiklayan <askalyan06@gmail.com>
2022-04-11 10:55:09 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from unicodedata import name
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    topic_name=models.CharField(primary_key=True,max_length=100)
+
+class Webpage(models.Model):
+    topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    url=models.URLField()
+
+class Access_Records(models.Model):
+    name=models.ForeignKey(Webpage,on_delete=models.CASCADE)
+    date=models.DateField()
